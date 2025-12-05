de85a1c5afb0d17492a26bd4e3aca21ea03ac9fc
Sebastien de Graffenried <seb.degraff@gmail.com>
2025-12-05 23:42:04 +0100

    separated sokol_gfx_types.h

---
+++ sokol_gfx_types.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef struct sg_buffer        { uint32_t id; } sg_buffer;
+typedef struct sg_image         { uint32_t id; } sg_image;
+typedef struct sg_sampler       { uint32_t id; } sg_sampler;
+typedef struct sg_shader        { uint32_t id; } sg_shader;
+typedef struct sg_pipeline      { uint32_t id; } sg_pipeline;
+typedef struct sg_attachments   { uint32_t id; } sg_attachments;
